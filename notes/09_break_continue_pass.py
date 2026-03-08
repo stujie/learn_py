62623c46186a8f471a1bf4dee87dba29e60805b7
Stutie <stutiebanerjie@gmail.com>
2026-03-08 01:38:00 -0500

    Update 09_break_continue_pass.py

--- notes/09_break_continue_pass.py
+++ notes/09_break_continue_pass.py
@@ -13,24 +13,3 @@ for i in range(0,10):
         break       # stop the loop completely
     print(i)
     
-
-while True:
-    name = input("Enter your name: ")
-    if name != "":
-        break
-print("Hello", name)
-
-phone_number = "123-456-7890"
-
-for i in phone_number:
-    if i == "-":
-        continue
-    print(i, end="")
-print()
-
-for i in range(1, 21):
-
-    if i == 13:
-        pass
-    else:
-        print(i)
