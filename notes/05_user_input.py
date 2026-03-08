ba377a549204fa417db534d393df8c1ac1701053
Stutie <stutiebanerjie@gmail.com>
2026-03-07 22:16:21 -0500

    Update 05_user_input.py

--- notes/05_user_input.py
+++ notes/05_user_input.py
@@ -3,6 +3,6 @@ name = input("What is your name?: ")
 order = input("What do you want to eat for lunch?: ")
 time = input("What time?: ")
 
-text = "Hey " + name + "! Wanna grab " + order + " for lunch at " + time + "?"
+send_text = "Hey " + name + "! Wanna grab " + order + " for lunch at " + time + "?"
 
-print(text)
+print(send_text)
