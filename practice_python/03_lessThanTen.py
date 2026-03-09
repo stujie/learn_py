c8576c9102c03eaa1ac5258dee5c64a3ea547fbf
Stutie <stutiebanerjie@gmail.com>
2026-03-09 15:59:23 -0400

    Update 03_lessThanTen.py

--- practice_python/03_lessThanTen.py
+++ practice_python/03_lessThanTen.py
@@ -1,10 +1,6 @@
 """
 Exercise 3: Take a list and write a program that prints out all the elements of the list that are less than 5.
 
-- Write this in one line of Python.
-
-- Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
-
 """
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
